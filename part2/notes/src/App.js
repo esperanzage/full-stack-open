00f9d86b32702ca46d6217e366377214c7a9c766
miniroo8 <miniroo8@users.noreply.github.com>
2021-03-18 20:08:09 +1300

    Part 2-1

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Note from './components/Note'
 
-function App() {
+
+// const NoteList = ({ notes }) => {
+//   return(
+//     <div><ul>{notes.map(note => <li key={note.id}>{note.content}</li>)}</ul></div>
+//   )
+// }
+
+
+const App = ({ notes }) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      {notes.map(note => <Note note={note} key={note.id} />)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
